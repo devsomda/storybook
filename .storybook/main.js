d45c750be0bf36308f79ade70e8aea16d8fb1dd6
Dasha <dasha@peoplcat.com>
2024-01-11 15:01:38 +0900

    chapter 1

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,37 +1,19 @@
-// /** @type { import('@storybook/react-vite').StorybookConfig } */
-// const config = {
-//   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-//   staticDirs: ["../public"],
-//   addons: [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-interactions",
-//   ],
-//   framework: {
-//     name: "@storybook/react-vite",
-//     options: {},
-//   },
-//   docs: {
-//     autodocs: "tag",
-//   },
-// };
-// export default config;
-
-
-module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-   staticDirs: ['../public'],
-   addons: [
-     '@storybook/addon-links',
-     '@storybook/addon-essentials',
-     '@storybook/preset-create-react-app',
-     '@storybook/addon-interactions',
-   ],
-   features: {
-     postcss: false,
-   },
-   framework: '@storybook/react',
-   core: {
-     builder: 'webpack4',
-   },
- };
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/components/**/*.stories.@(js|jsx)'],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
